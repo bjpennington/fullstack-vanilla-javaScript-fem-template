af5815da7668e5fd22a7f59fa4f4c4ec9a823ae7
Beth Pennington <bj.pennington@wetransfer.com>
2024-08-28 10:37:10 -0500

    checkpoint one

---
+++ src/shared/controller.js
@@ -0,0 +1,27 @@
+/**
+ * @typedef {import('./viewBase.js').default} View
+ */
+export default class Controller {
+  /** @type { View } */
+  #view;
+
+  /** @param {{ view: View }} deps */
+  constructor({ view }) {
+    this.#view = view;
+  }
+
+  static init(deps) {
+    const controller = new Controller(deps);
+    controller.#init();
+    return controller;
+  }
+
+  #init() {
+    const initialData = [
+      { name: 'Erick Wendel', age: 28, email: 'erick@erick.com' },
+      { name: 'Ana Neri', age: 24, email: 'ana@ana.com' },
+      { name: 'Marc Berg', age: 24, email: 'marc@marc.com' }
+    ]
+    this.#view.render(initialData)
+  }
+}
